ec264e4b93e11e9b68d5a541c4c8308cc0be3a5c
Joanne Ho <joaho@adobe.com>
2024-06-14 16:26:41 -0700

    modified footer block to test.

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -6,16 +6,27 @@ import { loadFragment } from '../fragment/fragment.js';
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  const footerMeta = getMetadata('footer');
-  block.textContent = '';
+  // const footerMeta = getMetadata('footer');
+  // block.textContent = '';
 
-  // load footer fragment
-  const footerPath = footerMeta.footer || '/footer';
+  // // load footer fragment
+  // const footerPath = footerMeta.footer || '/footer';
+  // const fragment = await loadFragment(footerPath);
+
+  // // decorate footer DOM
+  // const footer = document.createElement('div');
+  // footer.classList.add('wrapper');
+  // while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  // block.append(footer);
+  // load footer as fragment
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
+  block.textContent = '';
   const footer = document.createElement('div');
-  footer.classList.add('wrapper');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
