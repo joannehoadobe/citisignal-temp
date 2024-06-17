0018b8bfe4b1a89b08df7df41f722dc24d8adc7c
Joanne Ho <joaho@adobe.com>
2024-06-17 14:26:25 -0700

    header changes for commerce drop-ins

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,13 +1,14 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
 // Drop-in Providers
-import { render as cartProvider } from '@dropins/storefront-cart/render.js';
+/* eslint-disable */
+import { render as cartProvider } from './dropins/storefront-cart/render.js';
 
 // Drop-in Containers
-import MiniCart from '@dropins/storefront-cart/containers/MiniCart.js';
+import MiniCart from './dropins/storefront-cart/containers/MiniCart.js';
 
 // Drop-in Tools
-import { events } from '@dropins/tools/event-bus.js';
+import { events } from './dropins/tools/event-bus.js';
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
