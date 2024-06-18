3a971f4aa58d823b2d54277f6331ca4a53a1da7e
Joanne Ho <joaho@adobe.com>
2024-06-18 11:46:01 -0700

    temp comment out functions in editor-support.js to figure out why it won't load.

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -12,101 +12,101 @@ import { decorateRichtext } from './editor-support-rte.js';
 import { decorateMain } from './scripts.js';
 
 console.log('before functions in editor-support');
-async function applyChanges(event) {
-  console.log('in begging in applyChanges');
-  // redecorate default content and blocks on patches (in the properties rail)
-  const { detail } = event;
+// async function applyChanges(event) {
+//   console.log('in begging in applyChanges');
+//   // redecorate default content and blocks on patches (in the properties rail)
+//   const { detail } = event;
 
-  const resource = detail?.request?.target?.resource // update, patch components
-      || detail?.request?.target?.container?.resource // update, patch, add to sections
-      || detail?.request?.to?.container?.resource; // move in sections
-  if (!resource) return false;
-  const updates = detail?.response?.updates;
-  if (!updates.length) return false;
-  const { content } = updates[0];
-  if (!content) return false;
+//   const resource = detail?.request?.target?.resource // update, patch components
+//       || detail?.request?.target?.container?.resource // update, patch, add to sections
+//       || detail?.request?.to?.container?.resource; // move in sections
+//   if (!resource) return false;
+//   const updates = detail?.response?.updates;
+//   if (!updates.length) return false;
+//   const { content } = updates[0];
+//   if (!content) return false;
 
-  const parsedUpdate = new DOMParser().parseFromString(content, 'text/html');
-  const element = document.querySelector(`[data-aue-resource="${resource}"]`);
+//   const parsedUpdate = new DOMParser().parseFromString(content, 'text/html');
+//   const element = document.querySelector(`[data-aue-resource="${resource}"]`);
 
-  console.log('before IF element in applyChanges');
-  if (element) {
-    if (element.matches('main')) {
-      const newMain = parsedUpdate.querySelector(`[data-aue-resource="${resource}"]`);
-      newMain.style.display = 'none';
-      element.insertAdjacentElement('afterend', newMain);
-      decorateMain(newMain);
-      decorateRichtext(newMain);
-      await loadBlocks(newMain);
-      element.remove();
-      newMain.style.display = null;
-      // eslint-disable-next-line no-use-before-define
-      attachEventListners(newMain);
-      return true;
-    }
+//   console.log('before IF element in applyChanges');
+//   if (element) {
+//     if (element.matches('main')) {
+//       const newMain = parsedUpdate.querySelector(`[data-aue-resource="${resource}"]`);
+//       newMain.style.display = 'none';
+//       element.insertAdjacentElement('afterend', newMain);
+//       decorateMain(newMain);
+//       decorateRichtext(newMain);
+//       await loadBlocks(newMain);
+//       element.remove();
+//       newMain.style.display = null;
+//       // eslint-disable-next-line no-use-before-define
+//       attachEventListners(newMain);
+//       return true;
+//     }
 
-    console.log('before IF block in applyChanges');
-    const block = element.parentElement?.closest('.block[data-aue-resource]') || element?.closest('.block[data-aue-resource]');
-    if (block) {
-      const blockResource = block.getAttribute('data-aue-resource');
-      const newBlock = parsedUpdate.querySelector(`[data-aue-resource="${blockResource}"]`);
-      if (newBlock) {
-        newBlock.style.display = 'none';
-        block.insertAdjacentElement('afterend', newBlock);
-        decorateButtons(newBlock);
-        decorateIcons(newBlock);
-        decorateBlock(newBlock);
-        decorateRichtext(newBlock);
-        await loadBlock(newBlock);
-        block.remove();
-        newBlock.style.display = null;
-        return true;
-      }
-    } else {
-      // sections and default content, may be multiple in the case of richtext
-      const newElements = parsedUpdate.querySelectorAll(`[data-aue-resource="${resource}"],[data-richtext-resource="${resource}"]`);
-      if (newElements.length) {
-        const { parentElement } = element;
-        if (element.matches('.section')) {
-          const [newSection] = newElements;
-          newSection.style.display = 'none';
-          element.insertAdjacentElement('afterend', newSection);
-          decorateButtons(newSection);
-          decorateIcons(newSection);
-          decorateRichtext(newSection);
-          decorateSections(parentElement);
-          decorateBlocks(parentElement);
-          await loadBlocks(parentElement);
-          element.remove();
-          newSection.style.display = null;
-        } else {
-          element.replaceWith(...newElements);
-          decorateButtons(parentElement);
-          decorateIcons(parentElement);
-          decorateRichtext(parentElement);
-        }
-        return true;
-      }
-    }
-  }
+//     console.log('before IF block in applyChanges');
+//     const block = element.parentElement?.closest('.block[data-aue-resource]') || element?.closest('.block[data-aue-resource]');
+//     if (block) {
+//       const blockResource = block.getAttribute('data-aue-resource');
+//       const newBlock = parsedUpdate.querySelector(`[data-aue-resource="${blockResource}"]`);
+//       if (newBlock) {
+//         newBlock.style.display = 'none';
+//         block.insertAdjacentElement('afterend', newBlock);
+//         decorateButtons(newBlock);
+//         decorateIcons(newBlock);
+//         decorateBlock(newBlock);
+//         decorateRichtext(newBlock);
+//         await loadBlock(newBlock);
+//         block.remove();
+//         newBlock.style.display = null;
+//         return true;
+//       }
+//     } else {
+//       // sections and default content, may be multiple in the case of richtext
+//       const newElements = parsedUpdate.querySelectorAll(`[data-aue-resource="${resource}"],[data-richtext-resource="${resource}"]`);
+//       if (newElements.length) {
+//         const { parentElement } = element;
+//         if (element.matches('.section')) {
+//           const [newSection] = newElements;
+//           newSection.style.display = 'none';
+//           element.insertAdjacentElement('afterend', newSection);
+//           decorateButtons(newSection);
+//           decorateIcons(newSection);
+//           decorateRichtext(newSection);
+//           decorateSections(parentElement);
+//           decorateBlocks(parentElement);
+//           await loadBlocks(parentElement);
+//           element.remove();
+//           newSection.style.display = null;
+//         } else {
+//           element.replaceWith(...newElements);
+//           decorateButtons(parentElement);
+//           decorateIcons(parentElement);
+//           decorateRichtext(parentElement);
+//         }
+//         return true;
+//       }
+//     }
+//   }
 
-  console.log('end applyChanges');
-  return false;
-}
+//   console.log('end applyChanges');
+//   return false;
+// }
 
-function attachEventListners(main) {
-  console.log('beginning attachEventListeners');
-  [
-    'aue:content-patch',
-    'aue:content-update',
-    'aue:content-add',
-    'aue:content-move',
-    'aue:content-remove',
-  ].forEach((eventType) => main?.addEventListener(eventType, async (event) => {
-    event.stopPropagation();
-    const applied = await applyChanges(event);
-    if (!applied) window.location.reload();
-  }));
-}
+// function attachEventListners(main) {
+//   console.log('beginning attachEventListeners');
+//   [
+//     'aue:content-patch',
+//     'aue:content-update',
+//     'aue:content-add',
+//     'aue:content-move',
+//     'aue:content-remove',
+//   ].forEach((eventType) => main?.addEventListener(eventType, async (event) => {
+//     event.stopPropagation();
+//     const applied = await applyChanges(event);
+//     if (!applied) window.location.reload();
+//   }));
+// }
 
-attachEventListners(document.querySelector('main'));
+// attachEventListners(document.querySelector('main'));
