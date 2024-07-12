fdb171fb32dc59ff9d37ed030d5538c90b4cf0fb
Joanne Ho <joaho@adobe.com>
2024-07-12 09:54:10 -0700

    renable parameter for configs call, but breaks in x-walk.

--- scripts/configs.js
+++ scripts/configs.js
@@ -26,8 +26,7 @@ export const calcEnvironment = () => {
 function buildConfigURL(environment) {
   const env = environment || calcEnvironment();
   const configURL = new URL(`${window.location.origin}/configs.json`);
-  // configURL.searchParams.set('sheet', env);
-  configURL.searchParams.set('foo', env);
+  configURL.searchParams.set('sheet', env);
   return configURL;
 }
 
