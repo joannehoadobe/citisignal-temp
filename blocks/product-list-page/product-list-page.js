d88c48b5a5427275328741636e75848dab66ae1f
Natxo Cabré <cabre@adobe.com>
2024-07-24 12:15:59 +0200

    fixed duplicated sku on product URL for PLP page

--- blocks/product-list-page/product-list-page.js
+++ blocks/product-list-page/product-list-page.js
@@ -43,7 +43,7 @@ export default async function decorate(block) {
     context: {
       customerGroup: await getConfigValue('commerce-customer-group'),
     },
-    route: ({ sku, urlKey }) => `/products/${urlKey}/${sku}`,
+    route: ({ sku, urlKey }) => `/products/${sku}`,
   };
 
   if (type !== 'search') {
