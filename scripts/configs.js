0706aa21610be5e69d0553d29a9b90e9c1c4b921
Joanne Ho <joaho@adobe.com>
2024-07-29 11:00:11 -0700

    remove temp workaround for configs.js and try and debug if any more issues after latest merge

--- scripts/configs.js
+++ scripts/configs.js
@@ -33,12 +33,6 @@ function buildConfigURL(environment) {
     fileName = `configs-${env}.json`;
   }
   const configURL = new URL(`${window.location.origin}/${fileName}`);
-  /* eslint-disable-next-line max-len */
-  // const configURL = new URL(`${window.location.origin}/content/citisignal-edge-delivery/${fileName}`);
-  /* eslint-disable-next-line no-use-before-define */
-  if (getAemAuthorEnv()) {
-    return new URL(`${window.location.origin}/content/citisignal-edge-delivery/${fileName}`);
-  }
   return configURL;
 }
 
