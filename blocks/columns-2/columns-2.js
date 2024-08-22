4018d97b64a2ae4cc7d33d6620a1aa6a4c413a47
Joanne Ho <joaho@adobe.com>
2024-08-22 14:18:26 -0700

    not sure why files have to be renamed column-2.* when id is column2.

--- blocks/columns-2/columns-2.js
+++ blocks/columns-2/columns-2.js
@@ -38,6 +38,8 @@ export default function decorate(block) {
   });
 
   const isAemAuthor = getAemAuthorEnv();
+  /* eslint-disable-next-line no-console */
+  console.log(`in columns block, inner HTML = ${block.innerHTML}, text content = ${block.textContent}`);
   if (isAemAuthor && /^\s*\n\s*$/.test(block.innerHTML)) { // block.innerHTML.trim() === '' && block.childNodes && block.childNodes.length === 0) {
     /* eslint-disable-next-line no-console */
     console.log(`in columns block, inner HTML = ${block.innerHTML}, text content = ${block.textContent}`);
