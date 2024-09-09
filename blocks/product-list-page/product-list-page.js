bc046cbf897b015ff6effcb9ed40661fba1e36aa
Joanne Ho <joaho@adobe.com>
2024-09-09 14:16:40 -0700

    commented out specific case for AEM author/UE, test if PLP acts like PDP.

--- blocks/product-list-page/product-list-page.js
+++ blocks/product-list-page/product-list-page.js
@@ -4,13 +4,13 @@ import { getConfigValue, getAemAuthorEnv, getAemContentPath } from '../../script
 export default async function decorate(block) {
   const isAemAuthor = getAemAuthorEnv();
   if (isAemAuthor) {
-    // let authorContentPath = getAemContentPath();
-    // authorContentPath = `${authorContentPath}.resource/scripts/widgets/search.js`;
-    // await import(`${authorContentPath}`);
+    let authorContentPath = getAemContentPath();
+    authorContentPath = `${authorContentPath}.resource/scripts/widgets/search.js`;
+    await import(`${authorContentPath}`);
     // eslint-disable-next-line import/no-unresolved
-    await import('@widgets/search.js');
+    // await import('@widgets/search.js');
     // eslint-disable-next-line no-console
-    console.log('after calling to importmap for plp');
+    console.log('after calling with custom functions in configs.js for plp');
   } else {
     // eslint-disable-next-line import/no-absolute-path, import/no-unresolved
     await import('/scripts/widgets/search.js');
